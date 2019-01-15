2e1fa5ad47294e08af70cdbb9b5e665fd511a6a4
msp233 <937207392@qq.com>
2019-01-15 10:56:54 +0800

    jwt原生实现

--- extends/linux_jwt.php
+++ extends/linux_jwt.php
@@ -16,7 +16,7 @@ $payload = array(
     'iss' => 'msp',//jwt签发者
     'sub' => '0987123',//jwt面向的用户
     'aud' => 'www.baidu.com',//接收jwt的地方
-    'iat' => $time,//签发时间
+    'iat' => $time,//签名时间
     'nbf' => $time,//开始生效时间
     'exp' => $time+10,//过期时间，用unix时间戳
     'jti' => md5('id'),
@@ -24,12 +24,12 @@ $payload = array(
 
 //加密
 $token = jwt_encode($payload,$key);
-echo $token;
+//echo $token;
 
 try{
-    $decode_token = jwt_decode($token,$key,['jti'=>md5('id')]);
+    $decoded = jwt_decode($token,$key,['jti'=>md5('id')]);
 }catch (\Exception $e){
     $decoded = ['msg'=>'token不对'];
 }
-file_put_contents('linux.txt','token'.date('Y-m-d H:i:s',$time). "\n" . $token . "\n",8);
+file_put_contents('logs.txt','token'.date('Y-m-d H:i:s',$time). "\n" . $token . "\n",8);
 echo json_encode($decoded);
