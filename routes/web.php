cdc36c0e48c58867a522ea7688960dc47e39bf36
msp233 <937207392@qq.com>
2019-01-18 15:47:16 +0800

    oauth del mistake

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use Illuminate\Http\Request;
+
 //http://localhost:8080/blog/public/index.php
 Route::get('/', function () {
     return view('welcome');
@@ -19,3 +22,10 @@ Route::get('/', function () {
 Route::namespace('Wap')->group(function(){
     Route::get('/','IndexController')->name('index');
 });
+
+Route::get('oauth',function(){
+    return view('welcome');
+});
+//微信网页授权登录
+//https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect
+//https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx05b20198f8b779fb&redirect_uri=https://www.spalcw.com/weidian/public/index.php/oauth&response_type=code&scope=snsapi_base&state=msp#wechat_redirect
